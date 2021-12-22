5cc2185c727b93d06c736ab63541848412f49395
Gerald L. Amaya <Gerald L. Amaya@DESKTOP-R959GQK>
2021-12-22 16:26:10 -0600

    Agregar archivos de proyecto.

---
+++ Squeleton/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squeleton.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            int val1 = 0;
+            int val2 = 2;
+            int result = val1 + val2;
+
+            ViewBag.Message = "Your application description page." + result;
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "contacto de gato.";
+
+            return View();
+        }
+    }
+}
---
+++ Squeleton/Controllers/TenampaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squeleton.Controllers
+{
+    public class TenampaController : Controller
+    {
+        // GET: tenampa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
